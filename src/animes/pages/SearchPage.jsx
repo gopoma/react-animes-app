740449a65929b233022a0fab8348845e2eb4c3d6
gopoma <gordono@unsa.edu.pe>
2023-01-11 00:31:45 -0500

    Added messages on Empty and Error in Search

--- src/animes/pages/SearchPage.jsx
+++ src/animes/pages/SearchPage.jsx
@@ -8,19 +8,22 @@ export const SearchPage = () => {
     const location = useLocation();
     const {q = ""} = queryString.parse(location.search);
     const characters = getCharactersByName(q);
+    
+    const showSearch = q === "";
+    const showError = q !== "" && characters.length === 0;
 
     const {searchText, onInputChange} = useForm({
-        searchText: ""
+        searchText: q
     });
     const navigate = useNavigate();
     const onSearchSubmit = (event) => {
         event.preventDefault();
-        navigate(`/animes/search?q=${searchText}`);
+        navigate(`/animes/search?q=${searchText.trim()}`);
     };
 
     return (
-        <section className="container mx-auto flex flex-col gap-2 lg:flex-row lg:justify-center">
-            <section>
+        <section className="container mx-auto my-2 flex flex-col gap-2 lg:flex-row lg:justify-center">
+            <section className="bg-slate-200 border border-black p-2 flex flex-col gap-2 items-center">
                 <h1 className="text-4xl font-bold text-center p-4">Search a Character</h1>
                 <form 
                     className="w-11/12 mx-auto flex flex-col gap-2"
@@ -41,6 +44,18 @@ export const SearchPage = () => {
                         Search
                     </button>
                 </form>
+                <div
+                    className="animate__animated animate__fadeIn bg-blue-200 border border-blue-400 rounded-md px-3 py-2 text-blue-800"
+                    style={{display: (showSearch) ? "" : "none"}}
+                >
+                    Search a character
+                </div>
+                <div
+                    className="animate__animated animate__fadeIn bg-red-200 border border-red-400 rounded-md p-2 text-red-600"
+                    style={{display: (showError) ? "" : "none"}}
+                >
+                    No character with {q}
+                </div>
             </section>
             <section className="lg:grid lg:grid-cols-2 lg:gap-2">
                 {
